9c82329edc3f7ff64623bb9643351e6918717745
david04davidgr <davidgarcia.r.04@gmail.com>
2024-12-20 13:12:41 +0100

    Fixed prompt_id

--- src/main/java/es/viewnext/estimacion/mapper/MedicionPorPromptMapper.java
+++ src/main/java/es/viewnext/estimacion/mapper/MedicionPorPromptMapper.java
@@ -2,9 +2,11 @@ package es.viewnext.estimacion.mapper;
 
 import es.viewnext.estimacion.dto.MedicionPorPromptDTO;
 import es.viewnext.estimacion.model.MedicionPorPrompt;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface MedicionPorPromptMapper {
     MedicionPorPromptMapper INSTANCE = Mappers.getMapper(MedicionPorPromptMapper.class);
 
