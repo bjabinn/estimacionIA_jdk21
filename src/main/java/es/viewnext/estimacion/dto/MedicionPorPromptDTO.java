2b5cef59a5b9f2cdb2f968aa1947089f4fd0db1b
david04davidgr <davidgarcia.r.04@gmail.com>
2025-01-30 10:18:52 +0100

    Fixed relacion medicion-promptId

--- src/main/java/es/viewnext/estimacion/dto/MedicionPorPromptDTO.java
+++ src/main/java/es/viewnext/estimacion/dto/MedicionPorPromptDTO.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class MedicionPorPromptDTO {
     private Long promptId;
-    private String aplicaIa;
     private String usadaIa;
     private int calidadSalidaIa;
     private int estimacionSinIa;
